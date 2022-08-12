04e6d567e2e22b23fc1a99e6db063eb586e58a53
arunmass2005 <110777904+arunmass2005@users.noreply.github.com>
2022-08-12 15:31:00 +0530

    Update settings.py

--- web/web/settings.py
+++ web/web/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-54&*f_wiceg2&hxs=ei((@adcs&($o6-qizd#1%_i^!f5yn8uj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1','arunmasskr.com']
 
 
 # Application definition
